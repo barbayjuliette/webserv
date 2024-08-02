6c50a5f52e0a021a77fd31813a829746a63a67fc
Juliette Barbay <jbarbay@c2r6s12.42singapore.sg>
2024-08-02 20:08:19 +0800

    file upload working

--- cgi-bin/upload.py
+++ cgi-bin/upload.py
@@ -8,21 +8,15 @@ fileitem = form['file']
 print("Form keys: ", form.keys())
 
 if fileitem.filename:
-	# strip leading path from file name to avoid 
-	# directory traversal attacks
-	print('in IF')
 	fn = os.path.basename(fileitem.filename.replace("\\", "/" ))
 	file = open('./uploads/' + fn, 'wb')
-	print("this is fn: %s", fn)
-	print("this is file: %s", file)
 	file.write(fileitem.file.read())
 	message = 'The file "' + fn + '" was uploaded successfully'
    
 else:
-	print("in else")
 	message = 'No file was uploaded'
 
-print ("Content-type:plain/text\r\n\r\n")
+print ("Content-type:text/html\r\n\r\n")
 print ("<html>")
 print ('<head>')
 print ("<title>File upload</title>")
