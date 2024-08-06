2ce9b731c119efa938bf245306671699199225bc
Juliette Barbay <jbarbay@c2r6s12.42singapore.sg>
2024-08-06 22:36:48 +0800

    added some environment cgi

--- cgi-bin/checkbox.py
+++ cgi-bin/checkbox.py
@@ -4,9 +4,6 @@ from datetime import datetime
 
 form = cgi.FieldStorage()
 
-# if "blue" not in form:
-# 	print("<h1>Error</h1>")
-
 blue = form.getvalue('blue')
 red = form.getvalue('red')
 
@@ -16,15 +13,12 @@ if blue is None:
 if red is None:
 	red = 'off'
 
-now = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
-
 print ("Content-type:text/html\r\n\r\n")
 print ("<html>")
 print ('<head>')
 print ("<title>Hello - CGI Program</title>")
 print ('</head>')
 print ('<body>')
-print ("<h2>Today is %s</h2>" % (now))
 print ("<h2>Blue is  %s</h2>" % (blue))
 print ("<h2>Red is  %s</h2>" % (red))
 print ('</body>')
