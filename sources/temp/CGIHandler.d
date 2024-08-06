0bb9684f0e2d044bfbef89b5ce4f1db0cad2310e
Yun Xuan Liew <yliew@c2r6s14.42singapore.sg>
2024-08-06 14:35:54 +0800

    cgi refactored

--- sources/temp/CGIHandler.d
+++
@@ -1,6 +0,0 @@
-sources/temp/CGIHandler.o: sources/CGIHandler.cpp includes/CGIHandler.hpp \
-  includes/webserv.hpp
-
-includes/CGIHandler.hpp:
-
-includes/webserv.hpp:
--- sources/temp/ValidConfig.d
+++
@@ -1,6 +0,0 @@
-sources/temp/ValidConfig.o: sources/config/ValidConfig.cpp \
-  includes/config/ValidConfig.hpp includes/webserv.hpp
-
-includes/config/ValidConfig.hpp:
-
-includes/webserv.hpp:
